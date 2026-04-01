58f1cf57900eb10ad68a26056316a550f40fd6f8
manihirani894-hue <manihirani894@gmail.com>
2026-04-01 12:00:29 +0300

    update 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Signin from './components/Signin';
 import Getcars from './components/Getcars';
 import Addcars from './components/Addcars';
 import Mpesapayment from './components/Mpesapayment';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 
 function app(){
@@ -18,14 +18,14 @@ function app(){
      <div className='App-header'>
       <h1 className='text-warning bg-success'><b></b>WELCOME TO CARS PALACE</h1>
      </div>
-     <Navbar/>
-{/* <nav className='m-2'>
+     {/* <Navbar/> */}
+<nav className='m-2'>
   <Link to="/signup" className='btn btn-outline-success ms-2'id='su'>Signup</Link>
   <Link to="/signin" className='btn btn-outline-success ms-2'>Signin</Link>
   <Link to="/addcars" className='btn btn-outline-success ms-2' id='ap'>Add cars</Link>
   <Link to="/getcars" className='btn btn-outline-success ms-2'>Get cars</Link>
   
-</nav> */}
+</nav>
 
   
      <Routes>
